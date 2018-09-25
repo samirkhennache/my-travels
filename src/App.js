f40dece4b90ed4b05d9481de5c2e5be9f8756c12
samirkhennache <samirkhennache@gmail.com>
2018-09-25 13:38:32 +0200

    creation d'un tableau d'objet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Travel from './Travel'
+import Travels from './Travel'
 class App extends Component {
   render() {
     return (
@@ -10,27 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Samir KHENNACHE</h1>
         </header>
-        <Travel
-        image = "https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200"
-        caracter = "New York"
-        city = "New York"
-        country = "USA"
-        distance = "5834"
-        />
-         <Travel
-        image = "https://fifty-wp.s3.amazonaws.com/detours/uploads/2018/03/Barcelone-super-block-800x480.png"
-        caracter = "Barcelone"
-        city = "Barcelone"
-        country = "Espagne"
-        distance = "1033"
-        />
-         <Travel
-        image = "https://img.ev.mu/images/villes/6856/1605x642/6856.jpg"
-        caracter = "Rome"
-        city = "Rome"
-        country = "Italie"
-        distance = "1416"
-        />
+        <Travels />
       </div>
     );
   }
--- src/Travel.js
+++ src/Travel.js
@@ -8,4 +8,52 @@ const Travel =({image,caracter,distance,city,country}) =>(
         </figcaption>
     </figure>
 );
-export default Travel;
+const travels =[
+    {
+        image : "https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200",
+        caracter : "New York",
+        city : "New York",
+        country : "USA",
+        distance :"5834"
+    },
+    {
+        image : "https://fifty-wp.s3.amazonaws.com/detours/uploads/2018/03/Barcelone-super-block-800x480.png",
+        caracter : "Barcelone",
+        city : "Barcelone",
+        country : "Espagne",
+        distance : "1033"
+    },
+    {
+        image : "https://img.ev.mu/images/villes/6856/1605x642/6856.jpg",
+        caracter : "Rome",
+        city : "Rome",
+        country : "Italie",
+        distance : "1416"
+    },
+    {
+        image : "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Nizza-C%C3%B4te_d%27Azur.jpg/1200px-Nizza-C%C3%B4te_d%27Azur.jpg",
+        caracter : "Nice",
+        city : "Nice",
+        country : "France",
+        distance :"928"
+    },
+    {
+        image : "https://www.nacel.fr/medias/_cache/produits/289/imagePrincipale/1920_1440/ecole-de-langue-londres-tower-bridge.jpg",
+        caracter : "Londres",
+        city : "Londres",
+        country : "Grande Bretagne",
+        distance :"468"
+    },
+]
+
+ 
+const Travels = () => (
+    <div>
+      {travels.map((element,index) =>
+        // <Quote key = {index} quote={element.quote} image={element.image} character={element.character} />
+        <Travel key ={index} {...element} />
+      )}
+    </div>
+  );
+ 
+export default Travels;
